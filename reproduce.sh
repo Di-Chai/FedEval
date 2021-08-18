45ef782c808542f119e4f1ae58b9828cdf80af26
Di-Chai <dchai@cse.ust.hk>
2021-08-18 22:11:31 +0800

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 # MNIST IID
-python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i false -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i false -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m local -i false -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m local -i false -r 5 -e run
 
 # MNIST Non-IID
-python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 1 -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 2 -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 3 -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 1 -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 2 -r 5 -e run
-python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 3 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m local -i true -n 1 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m local -i true -n 2 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m local -i true -n 3 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m local -i true -n 1 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m local -i true -n 2 -r 5 -e run
+python3 -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m local -i true -n 3 -r 5 -e run
 
 # FEMNIST IID
-python3 -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m server -i false -r 5 -e run
-python3 -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m server -i false -r 5 -e run
+python3 -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m local -i false -r 5 -e run
+python3 -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m local -i false -r 5 -e run
 
 # FEMNIST Non-IID
-python3 -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m server -i true -r 5 -e run
-python3 -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m server -i true -r 5 -e run
+python3 -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m local -i true -r 5 -e run
+python3 -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m local -i true -r 5 -e run
 
 # CelebA IID
-python3 -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m server -i false -r 5 -e run
-python3 -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m server -i false -r 5 -e run
+python3 -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m local -i false -r 5 -e run
+python3 -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m local -i false -r 5 -e run
 
 # CelebA Non-IID
-python3 -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m server -i true -r 5 -e run
-python3 -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m server -i true -r 5 -e run
+python3 -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m local -i true -r 5 -e run
+python3 -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m local -i true -r 5 -e run
 
 # Sent140 IID
-python3 -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m server -i false -r 5 -e run
-python3 -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m server -i false -r 5 -e run
+python3 -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m local -i false -r 5 -e run
+python3 -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m local -i false -r 5 -e run
 
 # Sent140 Non-IID
-python3 -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m server -i true -r 5 -e run
-python3 -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m server -i true -r 5 -e run
+python3 -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m local -i true -r 5 -e run
+python3 -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m local -i true -r 5 -e run
 
