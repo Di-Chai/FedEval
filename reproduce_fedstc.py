1f895cb42b99bf8f9bc3563171eac4abac5088d6
Di-Chai <dchai@cse.ust.hk>
2021-08-21 00:30:18 +0800

    Create reproduce_fedstc.py

---
+++ reproduce_fedstc.py
@@ -0,0 +1,48 @@
+import os
+
+# MNIST IID
+# os.system("python -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i false -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i false -r 10 -e run")
+
+# MNIST Non-IID
+# os.system("python -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 1 -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 2 -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedSGD -c configs/workstation -m server -i true -n 3 -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 1 -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 2 -r 10 -e run")
+# os.system("python -W ignore trial.py -d mnist -s FedAvg -c configs/workstation -m server -i true -n 3 -r 10 -e run")
+
+# FEMNIST IID
+# os.system("python -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m server -i false -r 10 -e run")
+# os.system("python -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m server -i false -r 10 -e run")
+
+# FEMNIST Non-IID
+# os.system("python -W ignore trial.py -d femnist -s FedSGD -c configs/workstation -m server -i true -r 10 -e run")
+# os.system("python -W ignore trial.py -d femnist -s FedAvg -c configs/workstation -m server -i true -r 10 -e run")
+
+# CelebA IID
+# os.system("python -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m server -i false -r 10 -e run")
+# os.system("python -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m server -i false -r 10 -e run")
+
+# CelebA Non-IID
+# os.system("python -W ignore trial.py -d celeba -s FedSGD -c configs/workstation -m server -i true -r 10 -e run")
+# os.system("python -W ignore trial.py -d celeba -s FedAvg -c configs/workstation -m server -i true -r 10 -e run")
+
+# Sent140 IID
+# os.system("python -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m server -i false -r 10 -e run")
+# os.system("python -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m server -i false -r 10 -e run")
+
+# Sent140 Non-IID
+# os.system("python -W ignore trial.py -d semantic140 -s FedSGD -c configs/workstation -m server -i true -r 10 -e run")
+# os.system("python -W ignore trial.py -d semantic140 -s FedAvg -c configs/workstation -m server -i true -r 10 -e run")
+
+
+# FedSTC
+os.system("python -W ignore trial.py -d mnist -s FedSTC -c configs/local -m local -i false -r 5 -e run")
+os.system("python -W ignore trial.py -d mnist -s FedSTC -c configs/local -m local -i true -n 1 -r 5 -e run")
+os.system("python -W ignore trial.py -d femnist -s FedSTC -c configs/local -m local -i false -r 5 -e run")
+os.system("python -W ignore trial.py -d femnist -s FedSTC -c configs/local -m local -i true -r 5 -e run")
+os.system("python -W ignore trial.py -d celeba -s FedSTC -c configs/local -m local -i false -r 5 -e run")
+os.system("python -W ignore trial.py -d celeba -s FedSTC -c configs/local -m local -i true -r 5 -e run")
+os.system("python -W ignore trial.py -d semantic140 -s FedSTC -c configs/local -m local -i false -r 5 -e run")
+os.system("python -W ignore trial.py -d semantic140 -s FedSTC -c configs/local -m local -i true -r 5 -e run")
